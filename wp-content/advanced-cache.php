8975e2d7c3dc3520e138d15c719690ec3f16be25
nqlong <longnguyenqu@gmail.com>
2018-12-12 07:12:19 +0700

    facebook plugin

--- wp-content/advanced-cache.php
+++ wp-content/advanced-cache.php
@@ -2,11 +2,11 @@
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 define( 'WP_ROCKET_ADVANCED_CACHE', true );
-$rocket_cache_path = '/var/www/example.com/html/wp-content/cache/wp-rocket/';
-$rocket_config_path = '/var/www/example.com/html/wp-content/wp-rocket-config/';
+$rocket_cache_path = 'C:\xampp\htdocs\wp-tanoils/wp-content/cache/wp-rocket/';
+$rocket_config_path = 'C:\xampp\htdocs\wp-tanoils/wp-content/wp-rocket-config/';
 
-if ( file_exists( '/var/www/example.com/html/wp-content/plugins/wp-rocket/inc/front/process.php' ) ) {
-	include( '/var/www/example.com/html/wp-content/plugins/wp-rocket/inc/front/process.php' );
+if ( file_exists( 'C:\xampp\htdocs\wp-tanoils\wp-content\plugins\wp-rocket\inc\front/process.php' ) ) {
+	include( 'C:\xampp\htdocs\wp-tanoils\wp-content\plugins\wp-rocket\inc\front/process.php' );
 } else {
 	define( 'WP_ROCKET_ADVANCED_CACHE_PROBLEM', true );
 }
